5e2b06172a5ba7bf7b46dd097c02793d2e36cb84
adelebrgx <adele.bourgeix@gmail.com>
2019-05-03 17:08:17 -0400

    correcion

--- project/courses/models.py
+++ project/courses/models.py
@@ -2,3 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+class Course(models.Model):
+    CHOICES1 = (("Primavera", "Primavera"),("Otoño", "Otoño"),)
+    CHOICES2 = ((1,1),(2, 2),)
+    title=models.CharField(max_length=200)
+    code=models.CharField(max_length=20)
+    semester=models.CharField(choices=CHOICES1, default="Primavera", max_length=15)
+    section=models.IntegerField(choices=CHOICES2)
+    year=models.IntegerField()
+
+    def __str__(self):
+        return self.title
