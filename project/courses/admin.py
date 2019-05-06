399093532e3e1311d0acc57593adfdd7245f6277
rllullt <rllullt@gmail.com>
2019-05-06 17:05:09 -0400

    Agregados los modelos para estudiantes y equipos. Por alguna razón, al guardar un equipo se produce un error, que aun no veo, pero dice «field 'name' is not defined».

--- project/courses/admin.py
+++ project/courses/admin.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Course
+from .models import Course, Student, Team
 
 from django.contrib import admin
 
-admin.site.register(Course)
-
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Team)
