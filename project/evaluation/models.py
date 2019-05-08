00510cce5a69b439d3f05ce536c8361e5f3fc935
analiabs <analiabannura@gmail.com>
2019-05-08 18:08:49 -0300

    modelo evaluacion

---
+++ project/evaluation/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from courses.models import *
+
+# Create your models here.
+class Evaluation(models.Model):
+    name= models.CharField(max_length=200, primary_key=True)
+    init_date= models.DateField()
+    fin_date= models.DateField()
+    rubric= models.ForeignKey(Rubric, on_delete=models.SET_NULL, null=True)
+    state= models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Evaluation_Course(models.Model):
+    evaluation_name=models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    course=models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Evaluation_Account(models.Model):
+    evaluation_name=models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    account=models.ForeignKey(Account, on_delete=models.CASCADE)
+    team=models.ForeignKey(Team, on_delete=models.CASCADE)
+    student=models.ForeignKey(Student, on_delete=models.CASCADE)
