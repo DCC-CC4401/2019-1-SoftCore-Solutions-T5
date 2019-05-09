2610d96f3bcf4fae617ff7607212827aae72c7d6
adelebrgx <adele.bourgeix@gmail.com>
2019-05-09 12:38:26 -0400

    add courses

--- project/courses/views.py
+++ project/courses/views.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Course
+from .forms import CourseForm
 
 def courses_list(request):
-    courses= Course.objects.all().order_by('semester')
-    return render(request, 'courses/courses_list.html',{'courses':courses})
+    if request.method=='POST':
+        title = request.POST['title']
+        code= request.POST['code']
+        semester = request.POST['semester']
+        section = request.POST['section']
+        year = request.POST['year']
+        course = Course.objects.create(title = title, code = code,
+                                       semester = semester,
+                                       section = section, year = year)
+        course.save()
+        courses = Course.objects.all().order_by('semester')
+        return redirect('/courses', {'courses': courses})
+    courses = Course.objects.all().order_by('semester')
+    return render(request,'courses/courses_list.html', {'courses': courses})
