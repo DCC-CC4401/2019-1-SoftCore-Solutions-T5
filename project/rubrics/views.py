0fe7c61ef98fe3746e18c8af8790e617dce69174
lpavezb <lukas.pavez@ug.uchile.cl>
2019-05-26 02:12:41 -0400

    :cat:can add, modify and see details of rubrics, also validate unfilled fields:cat:

--- project/rubrics/views.py
+++ project/rubrics/views.py
@@ -18,12 +18,21 @@ def rubrics_list(request):
         ma = data['dur_max']
         t = data['table']
         t = t.replace('\t', '')
-        rows = t.split('\n')
-        rub = []
-        for row in rows[:len(rows)-1]:
-            r = row.split(',')
+        rows = t.split('%\n%')
+        rub = [rows[0].split('%,%')]
+        for row in rows[1:len(rows)]:
+            r = row.split('%,%')
             print(r)
-            rub.append(r)
+            if r[0] != '':
+                rub.append(r)
+
+        empty_column_indexs = []
+        for i in range(1, len(rub[0])):
+            if rub[0][i] == '':
+                empty_column_indexs.append(i)
+        for idx in empty_column_indexs[::-1]:
+            for row in rub:
+                row.pop(idx)
 
         rubric = Rubric.objects.all()
         create = True
@@ -57,6 +66,8 @@ def rubric_modify(request, rubric_key):
 
 def rubric_modify_database(request, rubric_key):
     rubric = Rubric.objects.get(name=rubric_key)
+    rubric.delete()
+
     if request.method == 'POST':
         data = request.POST
         name = data['name']
@@ -64,16 +75,28 @@ def rubric_modify_database(request, rubric_key):
         ma = data['dur_max']
         t = data['table']
         t = t.replace('\t', '')
-        rows = t.split('\n')
-        table = []
-        for row in rows[:len(rows) - 1]:
-            r = row.split(',')
-            table.append(r)
+        rows = t.split('%\n%')
+        rub = [rows[0].split('%,%')]
+        for row in rows[1:len(rows)]:
+            r = row.split('%,%')
+            if r[0] != '':
+                rub.append(r)
+        empty_column_indexs = []
+        for i in range(1, len(rub[0])):
+            if rub[0][i] == '':
+                empty_column_indexs.append(i)
+        for idx in empty_column_indexs[::-1]:
+            for row in rub:
+                row.pop(idx)
 
-        rubric.name = name
-        rubric.duration_min = mi
-        rubric.duration_max = ma
-        rubric.set_rubric(table)
-        rubric.save()
+        create = True
+        rubric = Rubric.objects.all()
+        for r in rubric:
+            if r.name == name:
+                create = False
+        if create:
+            ru = Rubric.objects.create(name=name, duration_min=mi, duration_max=ma, state=False)
+            ru.set_rubric(rub)
+            ru.save()
     rubrics = Rubric.objects.all()
     return render(request, 'rubrics/rubrics_list.html', {'rubrics': rubrics})
